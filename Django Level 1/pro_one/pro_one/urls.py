7a1ed12e62c0db732b645d30941c6d43c88606d0
guumee <guzelya93@gmail.com>
2020-04-18 17:25:28 +0300

    add app urls view using include() function

--- Django Level 1/pro_one/pro_one/urls.py
+++ Django Level 1/pro_one/pro_one/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
+from django.conf.urls import url, include
 from app_one import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
+    url(r'^help/', include('app_one.urls')),
+    path('admin/', admin.site.urls),
+
 ]
