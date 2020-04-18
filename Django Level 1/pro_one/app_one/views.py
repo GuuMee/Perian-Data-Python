dad29650f7f56ebc6387b2d2b7e4089d456cab19
guumee <guzelya93@gmail.com>
2020-04-18 17:25:49 +0300

    added help view

--- Django Level 1/pro_one/app_one/views.py
+++ Django Level 1/pro_one/app_one/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<em>My Second App</em>")
+    my_dict = {'insert_me':"Hello! I'm from views.py"}
+    return render(request, "app_one/index.html", context = my_dict)
+    #return HttpResponse("<em>My Second App</em>")
+
+def help(request):
+    helpdict = {'help_insert':'HELP PAGE'}
+    return render(request, 'app_one/help.html', context= helpdict)
